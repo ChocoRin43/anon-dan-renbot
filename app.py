763ffd48be8caef7654910a403b67dc4b94eab94
Royu <nostresv2811@gmail.com>
2024-12-18 13:23:50 +0700

    memperbaiki nekosia dan penambahan nekos untuk gif, dan perbagus embed

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import discord
 from discord.ext import commands
 import aiohttp
+import json
 
 # Masukkan token bot Anda
-DISCORD_BOT_TOKEN = "Masukan bot token mu"
+DISCORD_BOT_TOKEN = "" #masukan bot token mu
 
 intents = discord.Intents.default()
 intents.message_content = True  # Aktifkan intent untuk membaca isi pesan
 intents.members = True
 
 # Prefix untuk bot
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="+", intents=intents)
+botIcon = discord.File("./rin.jpeg", filename="rin.jpeg")
 
 @bot.event
 async def on_ready():
@@ -50,19 +52,24 @@ async def waifu(ctx, category: str = "sfw", image_type: str = "waifu"):
                 data = await response.json()
                 image_url = data.get("url")
                 embed = discord.Embed(title=f"{category.upper()} {image_type.capitalize()}", color=discord.Color.fuchsia())
+                embed.set_footer(text="Rin Bot | Disediakan oleh waifu.pics", icon_url="attachment://rin.jpeg")
                 embed.set_image(url=image_url)
-                await ctx.send(embed=embed)
+                await ctx.send(embed=embed, file=discord.File("rin.jpeg", filename="rin.jpeg"))
             else:
                 await ctx.send("Terjadi kesalahan saat mengambil gambar.")
 @bot.command()
-async def nekos(ctx, kategori: str = "waifu"):
+async def nekos(ctx, kategori: str ="catgirl;"):
     """
     Mengambil gambar dari API nekos.cat.
-    :param kategori: Kategori gambar (default: waifu).
+    :param kategori: Kategori gambar (default: catgirl).
+    catgirl, foxgirl, wolf-girl, animal-ears, tail, tail-with-ribbon, tail-from-under-skirt
+    cute, cuteness-is-justice, blue-archive, girl, young-girl, maid, maid-uniform, vtuber, w-sitting, lying-down, hands-forming-a-heart, wink, valentine, headphones
+    thigh-high-socks, knee-high-socks, white-tights, black-tights, heterochromia, uniform, sailor-uniform, hoodie, ribbon, white-hair, blue-hair, long-hair, blonde, blue-eyes, purple-eyes
+    swimwear, swimsuit, bikini, sea, swim-ring
     """
-    valid_kategori = ["waifu", "neko", "kitsune", "shinobu", "megumin", "maid", "cat", "catgirl", "blue-archive", "young-girl", "uniform", "sailor-uniform"]
-    if kategori not in valid_kategori:
-        await ctx.send(f"Kategori tidak valid. Coba salah satu dari: {', '.join(valid_kategori)}")
+    
+    if kategori not in ["catgirl", "foxgirl", "wolf-girl", "animal-ears", "tail", "tail-with-ribbon", "tail-from-under-skirtcute", "cuteness-is-justice", "blue-archive", "girl", "young-girl", "maid", "maid-uniform", "vtuber", "w-sitting", "lying-down", "hands-forming-a-heart", "wink", "valentine", "headphonesthigh-high-socks", "knee-high-socks", "white-tights", "black-tights", "heterochromia", "uniform", "sailor-uniform", "hoodie", "ribbon", "white-hair", "blue-hair", "long-hair", "blonde", "blue-eyes", "purple-eyes", "swimwear", "swimsuit", "bikini", "sea", "swim-ring"]:
+        await ctx.send("Tipe gambar tidak valid! Cek daftar tipe di nekos")
         return
 
     url = f"https://api.nekosia.cat/api/v1/images/{kategori}"
@@ -73,17 +80,52 @@ async def nekos(ctx, kategori: str = "waifu"):
                 image_url = data.get("image", {}).get("original", {}).get("url")
                 cat = data.get("category", {})
                 if image_url:
-                    bed = discord.Embed(title=f"Kategori: {cat}", color=discord.Color.fuchsia())
+                    bed = discord.Embed(title=f"Kategori: {cat}", color=discord.Color.fuchsia(), description="Rin Bot | Disediakan oleh nekos.cat")
+                    bed.set_footer(text="Rin Bot | Disediakan oleh nekosia.cat", icon_url="attachment://rin.jpeg")
                     bed.set_image(url=image_url)
-                    await ctx.send(embed=bed)
+                    await ctx.send(embed=bed, file=discord.File("rin.jpeg", filename="rin.jpeg"))
                 else:
                     await ctx.send("Tidak dapat menemukan gambar untuk kategori ini.")
             else:
                 await ctx.send("Terjadi kesalahan saat menghubungi API.")
+                await ctx.send(f"Pesan Kesalahan{url}")
 
 @bot.command()
-async def talitha(ctx):
-    await ctx.send("Talitha punya nya Rehan")
+async def gif(ctx, kategori: str ="baka;"):
+    """
+    Mengambil gambar dari API nekos.best.
+    :param kategori: Kategori gif (default: baka).
+    baka, bite, blush, bored, cry, cuddle, dance, facepalm, feed, handhold, handshake, happy, highfive, 
+    hug, kick,kiss, laugh, lurk, nod, nom, nope, pat, peck, poke, pout, punch, shoot, shrug, slap, sleep, 
+    smile, smug, stare, think, thumbsup, tickle, wave, wink, yawn, yeet
+    """
+    
+    if kategori not in ["baka", "cry", "bite", "blush", "bored", "cuddle", "dance", "facepalm", "feed", "handhold", "handshake", "happy", "highfive", "hug", "kick", "kiss", "laugh", "lurk", "nod", "nom", "nope", "pat", "peck", "poke", "pout", "punch", "shoot", "shrug", "slap", "sleep", "smile", "smug", "stare", "think", "thumbsup", "tickle", "wave", "wink", "yawn", "yeet"]:
+        await ctx.send("Tipe gif tidak valid! Cek daftar tipe di nekos.best")
+        return
+
+    url = f"https://nekos.best/api/v2/{kategori}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                animeName = data["results"][0]["anime_name"]
+                image_url = data["results"][0]["url"]
+                if image_url:
+                    bed = discord.Embed(title=f"Anime: {animeName}", color=discord.Color.fuchsia(),)
+                    bed.set_footer(text="Rin Bot | Disediakan oleh nekos.cat", icon_url="attachment://rin.jpeg")
+                    bed.set_image(url=image_url)
+                    await ctx.send(embed=bed, file=discord.File("rin.jpeg", filename="rin.jpeg"))
+                else:
+                    await ctx.send("Tidak dapat menemukan gambar untuk kategori ini.")
+            else:
+                await ctx.send("Terjadi kesalahan saat menghubungi API.")
+                await ctx.send(f"Pesan Kesalahan{url}")
+
+
+@bot.command()
+async def talita(ctx):
+    await ctx.send("Talita punya nya Rehan")
 
 # Command untuk melihat jumlah member
 @bot.command()
