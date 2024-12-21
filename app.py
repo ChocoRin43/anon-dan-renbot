afa947f7d5710a661417ac6db4b6cc45458a07e8
Royu <nostresv2811@gmail.com>
2024-12-21 19:50:14 +0700

    Change the api key to env

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 import discord
+from dotenv import load_dotenv
+import os
 from discord.ext import commands
 import aiohttp
 import random
 
-DISCORD_BOT_TOKEN = "" #Masukan bot token mu
+load_dotenv()
+DISCORD_BOT_TOKEN = os.getenv("API_KEY")
 
 intents = discord.Intents.default()
 intents.message_content = True 
