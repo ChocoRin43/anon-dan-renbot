9097739326db5f742e364d99ff5be6a6ac500d12
Royu <nostresv2811@gmail.com>
2024-12-25 07:52:18 +0700

    prvnt tag

--- app.py
+++ app.py
@@ -60,8 +60,13 @@ async def ping(interaction: discord.Interaction):
 
 @bot.tree.command(name="gel", description="Untuk mengambil gambar dari Gelbooru")
 async def gel(interaction: discord.Interaction, *, tags: str = "anime"):
-    lquery = tags.lower()
     await interaction.response.defer()
+    prvnt_tag = ["loli", "shota", "shotacon", "lolicon"]
+    is_prvnt = any(qr in lquery.lower() for qr in prvnt_tag)
+    if is_prvnt:
+        await interaction.followup.send("Dikarenakan discord memiliki peraturan yang sangat ketat, kami tidak toleransi dengan tag tersebut", delete_after=5)
+        return
+    lquery = tags.lower()
     view = GelbooruView(lquery)
     image_url = await fetch_gelbooru_image(lquery)
     pv_url = await fetch_gelbooru_image_pv(lquery)
